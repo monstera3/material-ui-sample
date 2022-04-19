b5ee313e16da1e03f31c240eba836b1832983c31
natsumi <aka.723n@gmail.com>
2022-04-19 12:31:59 +0900

    アイコンインストール

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import Button from '@mui/material/Button';
+import HomeIcon from '@mui/icons-material/Home';
+import { HomeRepairService } from '@mui/icons-material';
 
 
 export const App = () => {
   return(
     <>
+
+      <Button>Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+      <HomeIcon/>
+      <HomeRepairService/>
     </>
 
   );
