75b8f751f0b05f77355fc4809dd4fcb33d48baed
natsumi <aka.723n@gmail.com>
2022-05-03 16:30:11 +0900

    アイコンインストール

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,21 @@ import React from 'react';
 import Button from '@mui/material/Button';
 import HomeIcon from '@mui/icons-material/Home';
 import { HomeRepairService } from '@mui/icons-material';
+import styled from 'styled-components';
+
+
+
+const StyledButton = styled(Button)`
+  background-color: #000;
+  color: salmon;
+  box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
+  padding: 7px 14px;
+  border-radius: 50%;
+  &:hover {
+    background-color: #0cf;
+    color: #000;
+  }
+`;
 
 
 export const App = () => {
@@ -13,6 +28,7 @@ export const App = () => {
       <Button variant="outlined">Outlined</Button>
       <HomeIcon/>
       <HomeRepairService/>
+      <StyledButton>Customized</StyledButton>
     </>
 
   );
